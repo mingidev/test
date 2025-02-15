a35a5c29cc8350355d6e1c807f22ea0e6949d11a
mingi <mingikr@icloud.com>
2025-02-16 01:29:36 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>BANK OF KOREA</h1>
+          
+          
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+export default App;
