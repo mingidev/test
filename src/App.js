d22e74d694487f2d0e388b4f672bd036cb216e66
mingi <mingikr@icloud.com>
2025-02-16 14:28:55 +0900

    2025-02-16

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          <h1>이민기😍😘</h1>
-          <img src={imgLogo} alt='logo image' height="500" width="500"/>
+          <h1>😍😘</h1>
+          <img src={imgLogo} alt='logo image' height="380" width="380"/>
           
           
           
